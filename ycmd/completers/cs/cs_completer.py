20570ee538ac82ea9960b781cd7381b6ebb0aeea
Spencer G. Jones <spencer@egovmt.com>
2016-06-03 15:54:55 -0600

    Fix diagnostics in PY3

--- ycmd/completers/cs/cs_completer.py
+++ ycmd/completers/cs/cs_completer.py
@@ -22,6 +22,7 @@ from __future__ import print_function
 from __future__ import division
 from builtins import *  # noqa
 from future import standard_library
+from future.utils import iteritems
 standard_library.install_aliases()
 from future.utils import itervalues
 
@@ -689,7 +690,7 @@ class CsharpSolutionCompleter( object ):
 
 
   def _SetDiagnosticsInDiagStructure( self, diagnostics, filename, source ):
-    for key, line in self._diagnostic_store[ filename ][ source ].iteritems():
+    for key, line in iteritems(self._diagnostic_store[ filename ][ source ]):
       del line[:]
 
     for diagnostic in diagnostics:
@@ -697,7 +698,7 @@ class CsharpSolutionCompleter( object ):
 
   def _SetAllFileDiagnosticsInDiagStructure( self, diagnostics, source ):
     for filename in self._diagnostic_store:
-      for key, line in self._diagnostic_store[ filename ][ source ].iteritems():
+      for key, line in iteritems(self._diagnostic_store[ filename ][ source ]):
         del line[:]
 
     for diagnostic in diagnostics:
